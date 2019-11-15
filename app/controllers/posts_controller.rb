68624baa2a17bf680e327c2602754fb032dd10d2
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-15 15:26:44 -0800

    [ADD]: new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.creator = User.find_by username: 'Test' 
+
+    if @post.save
+      flash[:notice] = 'Your post was created.'
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit!
+  end
 end
