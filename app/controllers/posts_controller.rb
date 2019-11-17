e241be0c68e271973c5fa20556a37c137c7ae9ad
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-16 22:02:40 -0800

    [STYLES]: truncating and newlines

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by {|post| post.created_at}
   end
 
   def new
