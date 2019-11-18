19786392137ca69a2d93b79e6601d92773fdd954
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-17 22:35:15 -0800

    [FIX]: typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
   has_secure_password validations: false
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, on: create, length: {minimum: 5}
+  validates :password, presence: true, on: :create, length: {minimum: 5}
 end
