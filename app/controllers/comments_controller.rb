7b1df5196a2c93df8681291ea4dc727724173deb
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-18 14:56:27 -0800

    [ADD]: vote actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  def vote
+    @comment = Comment.find(params[:id])
+    existing_votes = @comment.votes.where(user_id: current_user.id)
+    if existing_votes.empty?
+      Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+    else
+      existing_votes.first.update(vote: params[:vote])
+    end
+    redirect_back fallback_location: root_path
+  end
+
   private
 
   def comment_params
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:show, :index]
 
   def index
@@ -36,6 +36,18 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def vote
+    # there should be at most 1 element in the array because we enforce
+    # uniqueness of user-voteable
+    existing_votes = @post.votes.where(user_id: current_user.id)
+    if existing_votes.empty?
+      Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+    else
+      existing_votes.first.update(vote: params[:vote])
+    end
+    redirect_back fallback_location: root_path
   end
 
   private
