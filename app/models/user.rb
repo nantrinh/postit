a8cce2d1051fdb217082c231a6737c877edd3b5c
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-17 20:01:30 -0800

    [ADD]: user actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   has_secure_password validations: false
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: create, length: {minimum: 5}
 end
