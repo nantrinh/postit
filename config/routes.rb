f9951c349898ac1d7931dd0c4e00077faadbfda7
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-18 12:15:18 -0800

    [ADD]: vote models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :posts, :categories, except: :destroy do
-    resources :comments, only: :create
+
+  resources :categories, except: :destroy
+
+  resources :posts, except: :destroy do
+    member do
+      post 'vote'
+    end
+
+    resources :comments, only: :create do
+      member do
+        post 'vote'
+      end
+    end
   end
 
   resources :users, only: [:show, :create, :edit, :update]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_224233) do
+ActiveRecord::Schema.define(version: 2019_11_18_195803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,17 @@ ActiveRecord::Schema.define(version: 2019_11_17_224233) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.boolean "vote"
+    t.bigint "user_id"
+    t.string "voteable_type"
+    t.integer "voteable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["voteable_type", "voteable_id"], name: "index_votes_on_voteable_type_and_voteable_id"
   end
 
 end
