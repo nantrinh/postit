586cb9ef0e6e75930f45996531ba1196472b24f4
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-23 12:13:30 -0800

    [REFACTOR]: extract voting code to a module

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,21 +3,10 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
 
-  def total_votes
-    self.upvotes - self.downvotes  
-  end
-
-  def upvotes
-    self.votes.where(vote: true).size
-  end
-
-  def downvotes
-    self.votes.where(vote: false).size
-  end
+  include Voteable
 end
