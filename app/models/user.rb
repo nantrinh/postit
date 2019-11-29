9ca6c5b36765db4da743abfb0f948eee60e690e0
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-29 11:58:14 -0800

    [FIX]: password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true, on: create, length: {minimum: 5}
+  validates :password, presence: true, on: :create, length: {minimum: 5}
 
 #  include Sluggable
 #  sluggable_column :username
